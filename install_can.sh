6e98b1ac6578d5f9b4d5a6fa4d6f67132d5e73a0
Anson <maote.liu@gmail.com>
2025-08-13 17:39:40 +0800

    update udev rules setting

--- install_can.sh
+++ install_can.sh
@@ -10,6 +10,11 @@ KERNEL=="ttyACM*", ATTRS{idVendor}=="1483", ATTRS{idProduct}=="1110",MODE:="0666
 KERNEL=="ttyACM*", ATTRS{idVendor}=="1483", ATTRS{idProduct}=="1111",MODE:="0666", SYMLINK+="USB2CAN1"
 KERNEL=="ttyACM*", ATTRS{idVendor}=="1483", ATTRS{idProduct}=="1112",MODE:="0666", SYMLINK+="USB2CAN2"
 KERNEL=="ttyACM*", ATTRS{idVendor}=="1483", ATTRS{idProduct}=="1113",MODE:="0666", SYMLINK+="USB2CAN3"
+
+# --- Best-effort: enable low_latency via setserial on any ttyUSB*/ttyACM*
+# (Some drivers ignore setserial; rule is still safe.)
+ACTION=="add", KERNEL=="ttyUSB*", RUN+="/usr/bin/setserial /dev/%k low_latency"
+ACTION=="add", KERNEL=="ttyACM*", RUN+="/usr/bin/setserial /dev/%k low_latency"
 EOF
 
 sudo udevadm control --reload-rules
@@ -17,27 +22,4 @@ sudo udevadm trigger
 
 echo "*********************************"
 echo "Udev rules applied."
-echo "*********************************"
-
-# Add setserial low_latency to ~/.bashrc if not already present
-BASHRC="$HOME/.bashrc"
-LOW_LATENCY_SNIPPET='
-# Automatically enable low_latency for all ttyUSB devices
-for dev in /dev/ttyUSB*; do
-    if [ -e "$dev" ]; then
-        sudo setserial "$dev" low_latency
-    fi
-done
-'
-
-if ! grep -q "low_latency" "$BASHRC"; then
-    echo "Adding low_latency script to $BASHRC..."
-    echo "$LOW_LATENCY_SNIPPET" >> "$BASHRC"
-else
-    echo "low_latency script already present in $BASHRC, skipping."
-fi
-
-echo "*********************************"
-echo "Done. Please restart your terminal or run:"
-echo "source ~/.bashrc"
 echo "*********************************"
